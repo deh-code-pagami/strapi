a0bbdfa02fe06ce2ed2104d64fb3a19332c8a3cc
Ruben <ruben.fileti.3@gmail.com>
2024-02-06 22:26:38 +0100

    Update strapi

--- src/api/group/routes/group.js
+++ src/api/group/routes/group.js
@@ -9,10 +9,10 @@ const { createCoreRouter } = require('@strapi/strapi').factories;
 module.exports = createCoreRouter('api::group.group', {
   config: {
     create: {
-      middlewares: ['global::initializeOwner']
+      middlewares: ['api::group.initialize-owner']
     },
     find: {
       middlewares: ['api::group.filter-groups']
-    }
+    },
   }
 });
