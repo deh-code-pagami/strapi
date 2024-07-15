037c1c8f7c1e2a7718d586bc1303ca3b16c5d382
Ruben <ruben.fileti.3@gmail.com>
2024-07-15 10:52:35 +0200

    Group relations are now only populated on findOne

--- src/middlewares/autoPopulateFields.js
+++ src/middlewares/autoPopulateFields.js
@@ -1,19 +1,22 @@
 /**
  * @param {string} entity
+ * @param {boolean} isForDetail
  * @returns
  */
-function getPopulateFields(entity) {
+function getPopulateFields(entity, isForDetail) {
   switch (entity) {
     case 'groups':
-      return {
-        users: { populate: getPopulateFields('group-users') },
-        transactions: { populate: getPopulateFields('transactions') }
-      };
+      return isForDetail ?
+        {
+          users: { populate: getPopulateFields('group-users', isForDetail) },
+          transactions: { populate: getPopulateFields('transactions', isForDetail) }
+        } :
+        {};
     case 'transactions':
       return {
-        group: true,
-        transactionMetas: { populate: getPopulateFields('transaction-metas') }
-      }
+          group: true,
+          transactionMetas: { populate: getPopulateFields('transaction-metas', isForDetail) }
+        };
     case 'group-users':
       return {
         user: true
@@ -34,8 +37,10 @@ function getPopulateFields(entity) {
  */
 module.exports = () => {
   return (ctx, next) => {
-    const entity = ctx.request.path.match(/api\/([^/]+)/)?.[1];
-    const populatedFields = getPopulateFields(entity);
+    const [ _match, entity, id ] = ctx.request.path.match(/^\/api\/([^/]+)\/?([^/]+)?/) || [];
+    const isForDetail = !!id
+
+    const populatedFields = getPopulateFields(entity, isForDetail);
 
     if (populatedFields) {
       ctx.query.populate = populatedFields;
